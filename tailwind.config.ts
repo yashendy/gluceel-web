84afe3b7658e79ae855747a70642f55965e34815
sunrise <yashendy@gmail.com>
2025-10-20 10:58:07 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
-import type { Config } from 'tailwindcss';
-
+import type { Config } from "tailwindcss";
 
 export default {
-content: [
-'./app/**/*.{ts,tsx}',
-'./components/**/*.{ts,tsx}'
-],
-theme: { extend: {} },
-plugins: [],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: []
 } satisfies Config;
