6dbf3010456ca943b624f3719c6da6b8239d2b03
sunrise <yashendy@gmail.com>
2025-10-20 12:55:53 +0300

    Update and rename next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: { allowedOrigins: ["*"] },
+  },
+};
+
+export default nextConfig;
