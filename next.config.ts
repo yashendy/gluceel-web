accc8659e566968f94a53c073d2487547c3166dc
sunrise <yashendy@gmail.com>
2025-10-20 10:56:56 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from 'next';
-
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-experimental: {
-serverActions: { allowedOrigins: ["*"] },
-},
+  reactStrictMode: true,
+  experimental: {
+    serverActions: { allowedOrigins: ["*"] }
+  }
 };
-
 
 export default nextConfig;
