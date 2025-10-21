4286589699a00fdccd83d5188be4506883a56b6f
sunrise <yashendy@gmail.com>
2025-10-21 15:29:12 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { serverActions: { allowedOrigins: ["*"] } },
-};
-export default nextConfig;
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+/* NOTE: This file should not be edited */
